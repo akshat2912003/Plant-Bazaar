8d5bcfadf29a7188dfb5329b2621653b4fe06c4d
Akshat singh <117257669+akshat2912003@users.noreply.github.com>
2025-01-27 21:09:51 +0530

    Update NewCollections.jsx

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -5,7 +5,7 @@ const NewCollections = () => {
 
   const [new_collection, setNew_collection] = useState([])
   useEffect(() =>{
-    fetch('http://localhost:4000/newcollections')
+    fetch('https://plant-bazaar.onrender.com/newcollections')
     .then((response) => response.json())
     .then((data) => setNew_collection(data))
   }, [])
