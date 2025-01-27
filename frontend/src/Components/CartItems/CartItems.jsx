ae0c9c733f0bb4808a7146c37e6d0e9a88ebd1ef
Akshat singh <117257669+akshat2912003@users.noreply.github.com>
2025-01-27 21:08:01 +0530

    Update CartItems.jsx

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -1,74 +1,3 @@
-// import React, { useContext } from 'react'
-// import './CartItems.css'
-// import { PlantContext } from '../../Context/PlantContext'
-// import remove_icon from '../Assets/cart_cross_icon.png'
-// const CartItems = () => {
-//     const {getTotalCartAmount, all_product, cartItems, removeFromCart} = useContext(PlantContext)
-//   return (
-//     <div className='cartitems'>
-//       <div className="cartitems-format-main">
-//         <p>Products</p>
-//         <p>Title</p>
-//         <p>Price</p>
-//         <p>Quantity</p>
-//         <p>Total</p>
-//         <p>Remove</p>
-//       </div>
-//       <hr />
-//       {all_product.map((e) =>{
-//         if(cartItems[e.id] > 0){
-//             return <div>
-//             <div className="cartitems-format cartitems-format-main">
-//                 <img src={e.image} alt="" className='carticon-product-icon'/>
-//                 <p>{e.name}</p>
-//                 <p>Rs. {e.new_price}</p>
-//                 <button className='cartitems-quantity'>{cartItems[e.id]}</button>
-//                 <p>Rs. {e.new_price*cartItems[e.id]}</p>
-//                 <img className='cartitems-remove-icon' src={remove_icon} onClick={() => {removeFromCart(e.id)}} alt="" />
-//             </div>
-//             <hr />
-//           </div>   
-//         }
-//         return null;
-//       })}
-//       <div className="cartitems-down">
-//         <div className="cartitems-total">
-//             <h1>cart Totals</h1>
-//             <div>
-//                 <div className="cartitems-total-item">
-//                    <p>Subtotal</p> 
-//                    <p>Rs. {getTotalCartAmount()}</p>
-//                 </div>
-//                 <hr />
-//                 <div className="cartitems-total-item">
-//                     <p>Shipping Fee</p>
-//                     <p>Free</p>
-//                 </div>
-//                 <hr />
-//                 <div className="cartitems-total-item">
-//                     <h3>Total</h3>
-//                     <h3>Rs. {getTotalCartAmount()}</h3>
-//                 </div>
-//             </div>
-//             <button>PROCEED TO CHECKOUT</button>
-//         </div>
-//         <div className="cartitems-promocode">
-//             <p>If you have a promo code, Enter it here</p>
-//             <div className="cartitems-promobox">
-//                 <input type="text" placeholder='promo code'/>
-//                 <button>APPLY</button>
-//             </div>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default CartItems
-
-
-
-
 
 import React, { useContext } from 'react';
 import './CartItems.css';
@@ -90,7 +19,7 @@ const CartItems = () => {
             quantity: cartItems[e.id],
         }));
 
-        const response = await fetch('http://localhost:4000/create-checkout-session', {
+        const response = await fetch('https://plant-bazaar.onrender.com/create-checkout-session', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ products }),
